48f7d5e7627f4bfba51febc70eb3046b35a08b27
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-09-08 10:20:02 +0200

    support for waiting for load neccessary element page, loading chats (for one day) and detection 'New chat'

--- chat_gpt_empty_chat_remover.py
+++ chat_gpt_empty_chat_remover.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from sys import argv
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from sys import argv, exc_info
+from time import sleep
 
 
 def get_pc_username():
@@ -19,8 +24,27 @@ def set_up_chrome(pc_username):
     return webdriver.Chrome(options=chrome_options)
 
 
+def wait_for_element(driver, find_by, name):
+    wait = WebDriverWait(driver, 10)
+    try:
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-projection-id="3"]')))
+        print(f'Name "{name}" found by method By.{find_by}')
+        sleep(1.5)
+    except TimeoutException:
+        print(f'Name "{name}" not found by method By.{find_by}')
+
+
 if __name__ == "__main__":
     pc_username = get_pc_username()
     driver = set_up_chrome(pc_username)
     driver.get("https://chat.openai.com/")
+
+    wait_for_element(driver, 'TAG_NAME', 'ol')
+    chats_list = driver.find_elements(By.TAG_NAME, 'ol')
+    chats = chats_list[0].find_elements(By.TAG_NAME, 'a')
+    for chat in chats:
+        if chat.text == 'New chat':
+            print(40*'-')
+        else:
+            print(chat.text)
     input()
